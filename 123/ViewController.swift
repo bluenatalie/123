7a1fc6990dc81ba2b5a8cb2013e988132c40dca0
Bluenatalie <natalie.work@gmail.com>
2019-07-22 19:30:45 +0800

    Initial Commit

---
+++ 123/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by Tingyi Lee on 7/22/19.
+//  Copyright © 2019 Tingyi Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
